460a111b7d8df76f3ce7e25d1e23ceb9f57a21bc
Alperen Caykus <36015843+tr0ublee@users.noreply.github.com>
2019-02-03 03:38:37 +0300

    Update comicdownloader.py

--- comicdownloader.py
+++ comicdownloader.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3 
 import getpass
+from time import sleep
 #WRITTEN BY tr0uble
 
 
@@ -9,7 +10,7 @@ import getpass
 									#MACROS
 USER=getpass.getuser()
 FIREFOX_COOKIES='/home/'+USER+'/.mozilla/firefox/pei4xq4c.default/cookies.sqlite'
-COMMAND1='wget -O /home/'+USER+'/Desktop/RawFiles/' 
+COMMAND1='wget -q -O /home/'+USER+'/Desktop/RawFiles/' 
 COMMAND2=' -U "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:63.0) Gecko/20100101 Firefox/63.0" '
 COMMAND3='--header="Accept: text/html" --header="Cookie: __cfduid='
 COMMAND4='" -np  '
@@ -18,12 +19,64 @@ COMPILED="/home/"+USER+"/Desktop/Comics/"
 LINERS="-----------------------------------------------------------------------------------"
 #------------------------------------------------------------------------------------------
 
+def readInput():
+	links=[]
+	while True:
+		count=0
+		print "\n"
+		print "Keep pasting the links of the comics you want to download"
+		print "Type q to start downloading"
+		print "Type 1 to print the list"
+		print "Type 2 to delete from the list"
+		print "\n"
+		url=raw_input()
+		if url=="Q" or url=='q':
+			return links
+			break
+		elif url=="1" :
+			print LINERS
+			print "Your List:"
+			for i in links : print i
+			print LINERS
+		elif url=="2" : 
+			print "\n"
+			for i in links  : 
+				print " "+str(count)+" - "+i
+				count+=1
+			print "Enter the index of the link you want to remove (Starts FROM 0 ! )"
+			print "press q to exit"
+			i=raw_input()
+			
+			if i=="q" or i=="Q":
+				continue
+			else:
+				links.pop(int(i))
+		else:
+			if url not in links: 
+				links.append(url)
+			else : print "Already in the list. Skipping...\n"
+def makeChoice():
+	print "\nIMPORTANT NOTES : "
+	print "****Use firefox when obtaining the comic links."
+	print "****This script will automatically shut down the firefox. If you have any unsaved work, SAVE IT ! "
+	count=0
+	title=""
+	decision=0
+	while True:
+		print "Would you like to download all comics of an URL or specific ones ? "
+		print "1-All Comics"
+		print "2-Specific Ones"
+		decision=raw_input()
+		if decision=="1" or decision=="2":
+			return decision
+		else :
+			print "Wrong input.Try Again.\n"
 def creator():
 	os.system("killall firefox")
 	if os.system("find ~/Desktop/RawFiles | grep -q ~/Desktop/RawFiles")!=0: 
 		print "Creating Directory"
-		os.system("mkdir RawFiles")
-	os.system("mkdir Comics")
+		os.system("mkdir ~/Desktop/RawFiles")
+	os.system("mkdir ~/Desktop/Comics")
 
 def getCookies():
 	cookies=[] #[__cfduid,cf_clearance]
@@ -62,7 +115,6 @@ def getTitle():
 def compileComic(title):
 	
 	os.system("rar -m0 a "+COMPILED+title+".cbr "+RAW_PATH+"*.jpg")
-	os.system("rm "+RAW_PATH+"*")
 	
 def getComics():
 	file=open("/home/"+USER+"/Desktop/RawFiles/main.html", "rw+")
@@ -76,57 +128,74 @@ def getComics():
 			line=line[index+6:]
 			line=line.strip()
 			line=line.replace("s1600","s0")
-			os.system("wget -O /home/"+USER+"/Desktop/RawFiles/"+str(count)+".jpg"+" "+line[:-3])
+			os.system("wget -q -O /home/"+USER+"/Desktop/RawFiles/"+str(count)+".jpg"+" "+line[:-3])
 			count+=1 
-def __main__():
 
-	print "\nIMPORTANT NOTES : "
-	print "1- Use firefox when obtaining the comic links."
-	print "2- This script will automatically shut down the firefox. If you have any unsaved work, SAVE IT ! "
-	count=0
-	links=[]
-	title=""
-	while True:
-		count=0
-		print "\n"
-		print "Keep pasting the links of the comics you want to download"
-		print "Type q to start downloading"
-		print "Type 1 to print the list"
-		print "Type 2 to delete from the list"
-		print "\n"
-		url=raw_input()
-		if url=="Q" or url=='q':
+def getLinks(string): #For whole issues
+	lock=0
+	link=""
+	for i in string:
+		if lock==1 and i==" ":
 			break
-		elif url=="1" :
-			print LINERS
-			print "Your List:"
-			for i in links : print i
-			print LINERS
-		elif url=="2" : 
-			print "\n"
-			for i in links  : 
-				print " "+str(count)+" - "+i
-				count+=1
-			print "Enter the index of the link you want to remove (Starts FROM 0 ! )"
-			print "press q to exit"
-			i=raw_input()
-			
-			if i=="q" or i=="Q":
-				continue
-			else:
-				links.pop(int(i))
-		else:
-			if url not in links: 
-				links.append(url)
-			else : print "Already in the list. Skipping...\n"
+		elif lock==0 and i=="=" :
+			lock=1
+		elif lock==1:
+			link+=i
+
+	return link
+def readFile(): # For whole issues
+	result=[]
+	file=open("/home/"+USER+"/Desktop/RawFiles/main1.html", "rw+")
+	lock=0
+	for i in file :
+		if lock==1 and "</table>" in i :
+
+			return result
+
+		elif lock==1:
+			if "href=" in i :
+				result.append("https://readcomiconline.to"+getLinks(i))
+
+		elif lock==0 and '<table class="listing">' in i :
+			lock=1 
+
+
+def __main__():
+	decision=makeChoice()
+	links=readInput()
 	if links==[]: 
 		print "You didn't enter any link.Terminating..."
 		return 0
 	creator()
+	cf_clearance,cfduid=getCookies()[1],getCookies()[0]
 	for i in links:
-		os.system(COMMAND1+'main.html'+COMMAND2+COMMAND3+getCookies()[0]+'; cf_clearance='+getCookies()[1]+COMMAND4+i)
-		title=getTitle() 
-		getComics()
-		compileComic(title)
+		if decision=="2": #Specific issues
+			print "Downloading index"
+			os.system(COMMAND1+'main.html'+COMMAND2+COMMAND3+cfduid+'; cf_clearance='+cf_clearance+COMMAND4+i)
+			title=getTitle()
+			print "Downloading "+title
+			getComics()
+			compileComic(title)
+		else: #whole issues
+			print "Downloading index"
+			os.system(COMMAND1+'main1.html'+COMMAND2+COMMAND3+cfduid+'; cf_clearance='+cf_clearance+COMMAND4+i)
+			readFile()
+			wholeLinks=readFile()
+
+			if wholeLinks==[] : 
+				break
+			wholeLinks=wholeLinks[-1:]
+			for j in wholeLinks:
+				print "Downloading index"
+				os.system(COMMAND1+'main.html'+COMMAND2+COMMAND3+cfduid+'; cf_clearance='+cf_clearance+COMMAND4+j)
+				title=getTitle()
+				print "Downloading "+title
+				getComics()
+				compileComic(title)
+				os.system("find "+RAW_PATH+" ! -name 'main1.html' -type f -exec rm -f {} +")
+			os.system("rm "+RAW_PATH+"*")
+	os.system("rm "+RAW_PATH+"*")
+
+
 
 __main__()
